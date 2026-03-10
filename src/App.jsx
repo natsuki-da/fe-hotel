f324db3e47cd951eb9fbc7fa7bd4ecf5af514b66
Natsuki Iida <natsuki.iida0511@gmail.com>
2026-03-10 17:12:13 +0100

    add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/room-list" element={<RoomList />} />
-          <Route path="/room/`${roomId}`" element={<RoomDetail />} />
+          <Route path="/room/:roomId" element={<RoomDetail />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
---
+++ src/styles/fonts.js
@@ -0,0 +1,4 @@
+export const fonts = {
+    primary: "'Roboto Flex', sans-serif",
+    secondary: "'Alumni Sans Pinstripe', sans-serif"
+}
