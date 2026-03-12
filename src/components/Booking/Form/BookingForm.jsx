09e7ae158ceb48f184333c6f64e02cdca68143e6
Natsuki Iida <natsuki.iida0511@gmail.com>
2026-03-12 14:46:42 +0100

    add BookingForm component and design

---
+++ src/components/Booking/Form/BookingForm.jsx
@@ -0,0 +1,11 @@
+import * as S from "./BookingForm.styles"
+
+const BookingForm = () => {
+    return (
+        <S.Container>Hello
+            <S.FormWrapper>FORM</S.FormWrapper>
+        </S.Container>
+    )
+}
+
+export default BookingForm;
---
+++ src/components/Booking/Form/BookingForm.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+   background-color: rgba(130, 102, 36, 0.4);
+   display: flex;
+   flex-direction: column;
+`
+
+export const FormWrapper = styled.div`
+  width: 50%;
+  height: 30rem;
+  background-color: rgba(225, 76, 126, 0.4);
+`
+
+
